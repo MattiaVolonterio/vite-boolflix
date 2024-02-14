b7a03f4c15a2dd8fb7aba7d2ae68309ecc9cec1c
MattiaVolonterio <volonteriomattia@gmail.com>
2024-02-14 16:30:38 +0100

    feat: axios request, print list with movie

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,77 @@
 <script>
+import { store } from "./store";
+import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+      store,
+      searchForm: "",
+      flagURL: "",
+    };
   },
-  props: {},
-  methods: {},
+
+  computed: {
+    searchQuery() {
+      return `&query=${this.searchForm}`;
+    },
+  },
+
+  methods: {
+    fetchMovie() {
+      axios
+        .get(
+          `${store.movieURI}${store.apiKey}${this.searchQuery}${store.italianLang}`
+        )
+        .then((result) => {
+          store.requestMovie = result.data.results;
+          this.searchForm = "";
+        });
+    },
+
+    fetchSeries() {
+      axios
+        .get(
+          `${store.seriesURI}${store.apiKey}${this.searchQuery}${store.italianLang}`
+        )
+        .then((result) => {
+          store.requestSeries = result.data.results;
+          this.searchForm = "";
+        });
+    },
+
+    getFlag(lang) {
+      if (lang == "en" || lang == "it") {
+        return new URL(`./assets/flags/${lang}.svg`, import.meta.url).href;
+      } else {
+        return new URL(`./assets/flags/other.svg`, import.meta.url).href;
+      }
+    },
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="search-form">
+    <input v-model="searchForm" type="text" placeholder="Search movie" />
+    <button @click="fetchMovie()">Cerca</button>
+  </div>
+  <div class="lists-result">
+    <ul v-for="movie in store.requestMovie">
+      <li>{{ "Titolo: " + movie.title }}</li>
+      <li>{{ "Titolo originale: " + movie.original_title }}</li>
+      <li>
+        <img
+          :src="getFlag(movie.original_language)"
+          alt="flag"
+          class="flag-image"
+        />
+        {{ "Lingua originale: " + movie.original_language }}
+      </li>
+
+      <li>{{ "Valutazione: " + movie.vote_average }}</li>
+    </ul>
+  </div>
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss";
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,10 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  movieURI: "https://api.themoviedb.org/3/search/movie",
+  seriesURI: "https://api.themoviedb.org/3/search/tv",
+  apiKey: "?api_key=379781a4fe3768d4286069a61fcb5b6e",
+  italianLang: "&language=it-IT",
+  requestMovie: [],
+  requestSeries: [],
+});
