569949268dad62f752790ef244570cf4cd510b24
MattiaVolonterio <volonteriomattia@gmail.com>
2024-02-14 14:18:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
